c7ad918940d98414b2d847bedf7936debbd0f66c
vecheer <1571462409@qq.com>
2022-06-14 12:22:04 +0800

    :D

---
+++ CH3_running_data_area/src/main/java/L7_compressedOop/TestMain.java
@@ -0,0 +1,16 @@
+package L7_compressedOop;
+
+import org.openjdk.jol.info.ClassLayout;
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("[-XX:-UseCompressedOops]关闭了指针压缩");
+
+        Object o =  new Object();
+        System.out.println(o);
+        System.out.println(ClassLayout.parseInstance(o).toPrintable());
+
+
+        //System.out.println("[-XX:+UseCompressedClassPointers]开启了Class对象压缩");
+    }
+}
